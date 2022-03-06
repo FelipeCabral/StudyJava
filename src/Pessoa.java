5978ded8ba14b5c5bdb459e641871f5d9283d41e
FelipeCabralViana <felipecv7@gmail.com>
2022-03-06 12:09:20 -0300

    first commit

---
+++ src/Pessoa.java
@@ -0,0 +1,33 @@
+
+public class Pessoa {
+	
+	
+	
+	public static void main(String[]args) {
+		
+		String nome = "Felipe";
+		int idade = 15;
+		
+		while(idade!=18) {
+			
+			System.out.println("Yes");
+			break;
+			
+		}
+		
+		System.out.println(nome);
+		
+		if(idade>18) {
+			
+			System.out.println("Não vai entrar!");
+			
+		}
+		else {
+			
+			System.out.println("Identidade falsa!");
+		}
+		
+	}
+	
+
+}
